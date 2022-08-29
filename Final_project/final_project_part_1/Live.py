701bc323326783ff5dab779c8f2b200dc9b41409
sarahkossmann <sarah_kossmann@bitbucket.org>
2022-08-29 18:52:13 +0300

    fixed the error in games if user enter a letter instead of a number

--- Final_project/final_project_part_1/Live.py
+++ Final_project/final_project_part_1/Live.py
@@ -4,7 +4,7 @@ from games import *
 def welcome(name):
     print(f"Hello {name} and welcome to the World of Games (WoG). \nHere you can find many cool games to play.")
 
-
+# TODO remove the recursive
 def load_game():
     print("Please choose a game to play: \n1. Memory Game - a sequence of numbers will appear for 1 second and you have to guess it back \n2. Guess Game - guess a number and see if you chose like the computer \n3. Currency Roulette - try and guess the value of a random amount of USD in CAD")
     game_chosen = int(input("Type a number between 1 and 3: "))
--- Final_project/final_project_part_1/games.py
+++ Final_project/final_project_part_1/games.py
@@ -5,30 +5,59 @@ from datetime import date
 
 
 def memory_game():
+    attempts = 3
     print("A sequence of numbers will appear for one second. Remember them.")
     sleep(3)
     sequence_of_numbers = [(randint(1, 100)) for i in range(3)]
-    print(sequence_of_numbers, end="")
-    sleep(1)
-    print(" " * len(sequence_of_numbers), end="\r")
-    sleep(1)
-    user_answer_number_a = input("Guess the numbers you just saw (in the right order). \nProvide list of numbers separated by comma, e.g. 1,2,3. \nType the sequence here: ")
-    user_list_of_int = list(map(float, user_answer_number_a.split(',')))
 
-    if sequence_of_numbers == user_list_of_int:
-        print("Congrats! You guessed the right sequence.")
-    else:
-        print("Wrong answer. Please try again")
-        sleep(2)
-        memory_game()
+    while True:
+        while attempts > 0:
+            sleep(2)
+            print(sequence_of_numbers, end="")
+            sleep(1)
+            print(" " * len(sequence_of_numbers), end="\r")
+            sleep(1)
+
+            user_answer_number = str(input("Guess the numbers you just saw (in the right order). \nProvide list of numbers separated by comma, e.g. 1,2,3. \nType the sequence here: "))
+            user_answer_split = user_answer_number.split(',')
+
+            if len(user_answer_split) != 3:
+                print('Please enter only 3 numbers')
+                continue
+
+            flag=True
+            for elem in user_answer_split:
+                if not elem.isnumeric():
+                    flag=False
+                    break
+            if not flag:
+                print('Please enter only numbers')
+                break
+
+            for i in range(len(sequence_of_numbers)):
+                sequence_of_numbers[i]=str(sequence_of_numbers[i])
+            if sequence_of_numbers==user_answer_split:
+                print('Congrats! You won!')
+                exit()
+            else:
+                attempts-=1
+                print(f"You have {attempts} attempt(s) left.")
+
+        if attempts == 0:
+           print('Game over')
+           exit()
+
+memory_game()
 
 def guess_game():
     attempts = 5
-    computer_number = randint(1, 100)
+    computer_number = str(randint(1, 100))
 
     while attempts > 0:
-        user_number = int(input("Choose a number between 1 and 100: "))
-        if computer_number != user_number:
+        user_number = input("Guess the number the computer is thinking about. Choose a number between 1 and 100: ")
+        if not user_number.isnumeric():
+            print("Error. You should enter a numeric character")
+        elif computer_number != user_number:
             print("You didn't guess the computer number.")
             attempts += -1
             print(f"You have {attempts} attempt(s) left.")
@@ -40,6 +69,7 @@ def guess_game():
         else:
             print("You guessed right!")
             exit()
+
 
 def currency_roulette():
     today = date.today()
